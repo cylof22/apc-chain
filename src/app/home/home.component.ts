8abcc622ca936ad38d3d027b5c7024bd4d24595e
mucai <mu.cai@hotmail.com>
2018-06-05 17:57:48 +0800

    ui change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isMobile: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    var userAgent = navigator.userAgent;
+    if (userAgent.indexOf('Mobile') > -1 ||
+        userAgent.indexOf('Android') > -1 || 
+        userAgent.indexOf('iPhone') > -1) {
+          this.isMobile = true;
+    } 
   }
 
+  getBackGroundImage() {
+    var userAgent = navigator.userAgent;
+    alert(userAgent);
+    if (userAgent.indexOf('Mobile') > -1 ||
+        userAgent.indexOf('Android') > -1 || 
+        userAgent.indexOf('iPhone') > -1) {
+          alert('home');
+          return 'url("../assets/home.jpg")';
+    } else {
+      alert('home2');
+      return 'url("../assets/home2.png")';
+    }
+  }
 }
