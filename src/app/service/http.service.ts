fe74e4de0f1c1c6d57079cf7156600a94bb6fdc6
mucai <mu.cai@hotmail.com>
2018-06-08 16:11:16 +0800

    remove unused code

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -4,14 +4,11 @@ import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class HTTPService {
-    private url : string;
-
     constructor(private http: HttpClient) {
-        this.url = 'http://localhost:8000/#';
     }
 
     register(postedData : any) : Observable<string> {
         let body = postedData;
-        return this.http.post<string>(this.url + "/token", body);
+        return this.http.post<string>("/token", body);
     }
 }
